999b648fffa2f114011344ea63eb5f703b6a3a23
Ivan Sopov <sopov.ivan@gmail.com>
2018-04-11 22:05:52 +0300

    попробуем и на circleci строить для альтернативы

---
+++ .ci/prepare_postgres.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+psql -c "create user sample22plain with password 'sample22plain';" -U postgres
+psql -c "create database sample22plain;" -U postgres
+psql -c "grant all on database sample22plain to sample22plain;" -U postgres
+
+psql -c "create user sample22spring with password 'sample22spring';" -U postgres
+psql -c "create database sample22spring;" -U postgres
+psql -c "grant all on database sample22spring to sample22spring;" -U postgres
+
+psql -c "create user sample22jpa with password 'sample22jpa';" -U postgres
+psql -c "create database sample22jpa;" -U postgres
+psql -c "grant all on database sample22jpa to sample22jpa;" -U postgres
